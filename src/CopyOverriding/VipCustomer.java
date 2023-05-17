2f508f45135253f7ac17a8699d5093b2d6c3cb67
thinkisall <thinkisall@naver.com>
2023-05-18 06:56:47 +0900

    overriding에서 가상메서드로

---
+++ src/CopyOverriding/VipCustomer.java
@@ -0,0 +1,25 @@
+package CopyOverriding;
+
+public class VipCustomer extends Customer{
+
+    double saleRatio;
+
+    public VipCustomer(int customerID, String customerName, int bonusPoint) {
+        super(customerID, customerName,bonusPoint);
+        this.bonusPoint = bonusPoint;
+        customerGrade = "VIP";
+        bonusRatio = 0.05;
+        saleRatio = 0.1;
+    }    
+    
+    @Override
+    public int calcPrice(int price) {
+        bonusPoint += price * bonusRatio;
+        return price -= (int)(price * saleRatio);
+    }
+
+    public String getAgentID(){
+        return agentID;
+    }
+
+}
