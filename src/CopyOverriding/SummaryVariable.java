11a0e272531745b3a2c60182e3c667d9a1bfa5e7
thinkisall <thinkisall@naver.com>
2023-05-18 16:40:47 +0900

    변수,클래스 정리 -완-

--- src/CopyOverriding/SummaryVariable.java
+++ src/CopyOverriding/SummaryVariable.java
@@ -2,38 +2,34 @@ package CopyOverriding;
 
 public class SummaryVariable {
     
-    int 인스턴스변수;
-    String 인스턴스변수는="heap에 쌓입니다.";
+    static String 클래스변수="클래스 전체에서 사용가능한 변수";
     
-    static int 클래스변수;
-    static String 클래스변수는="프로그램이 종료되기전까지 메모리에 남아있어요";
-
-    static void bbb(){
-        String 참조변수지만함수안에써서지역변수 = "변수";
-        System.out.println(참조변수지만함수안에써서지역변수);
-    }
-
-    void 인스턴스메서드(){
-        int 인스턴스안에지역변수 = 1;
-        String 스트링은참조인데어디에쌓일까 = "어딜까";
-        System.out.println("인스턴스안에 지역변수는 stack에 쌓임");
-        System.out.println("놀랍게도 Stack에 쌓임 지역변수라서!");
-        System.out.println(인스턴스안에지역변수+스트링은참조인데어디에쌓일까);
-    }
+    String 인스턴스변수혹은필드="인스턴스 변수. heap에 쌓임.";
 
     static void 클래스메서드(){
-        System.out.println("Array나 Math같은 라이브러리들이 이걸로 만들어짐");
+        String 지역변수 = "변수";
     }
 
-    public static void main(String[] args) {
-        String args는뭘까 = "매개변수로써 지역변수에 해당";
-       SummaryVariable t1 = new SummaryVariable();
-        System.out.println("t1은 지역변수 물론 t2도");
-        t1.인스턴스메서드();
-       SummaryVariable t2 = new SummaryVariable();
-        t2.인스턴스메서드();
+    void 메서드(){
+        int 지역변수 = 1;
+        System.out.println("지역변수는 메서드가 호출되면 stack에 같이 올라간다.");
+    }
 
-        System.out.println(args는뭘까);
+
+    public static void main(String[] args) {
+        System.out.println("Main메서드는 entrypoint 클래스");
+        String args는뭘까 = "매개변수로써 지역변수에 해당";
+
+       SummaryVariable t1 = new SummaryVariable();
+       SummaryVariable t2 = new SummaryVariable();
+       
+       System.out.println("t1,t2는 지역변수");
+       System.out.println("생성자또한 메서드이기때문에 똑같이 메서드영역에 잡힌다.");
+       System.out.println("호출될때는 생성자가 호출되고 stack에 변수는 올라가고 값은 heap으로 적용");
+       t1.메서드();
+       t2.메서드();
+
+       System.out.println(args는뭘까);
     }
 
 }
